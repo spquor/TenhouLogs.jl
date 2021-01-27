2ee87f61166a055bf2f4b9930bd4feb43cc50cf6
spquor <spquor@gmail.com>
2021-01-27 20:37:47 +0300

    better datatypes for parser

--- analyse.jl
+++ analyse.jl
@@ -8,10 +8,10 @@ mutable struct PlayState
     scores:: Vector{Points}
     hands:: Vector{Hand}
     ponds:: Vector{Pond}
-    melds:: Vector{Meld}
+    melds:: Vector{Melds}
 end
 
-function analyseLog(log::String)
+function analyseLog(log::AbstractString)
 
     rx = r"<(?<tag>[A-Z]+)(?<str>.+?)\/>"s
     command = eachmatch(rx, log)
@@ -36,7 +36,7 @@ function analyseLog(log::String)
         Vector{Points}(undef, 4),
         Vector{Hand}(undef, 4),
         Vector{Pond}(undef, 4),
-        Vector{Meld}(undef, 4)
+        Vector{Melds}(undef, 4)
     )
 
     while (nextnode != nothing)
@@ -53,8 +53,8 @@ function analyseLog(log::String)
         while true
 
             if cmd[:tag] in ["T", "U", "V", "W", "D", "E", "F", "G"]
-                draw = Tile(cmd[:str])
-                
+                draw = Wall[cmd[:str]]
+
             elseif cmd[:tag] == "N"
                 #call meld
             elseif cmd[:tag] == "REACH"
@@ -69,7 +69,7 @@ function analyseLog(log::String)
                 #tie
                 nextnode = iterate(command, state)
                 break
-            else continue end
+            else error("Invalid tag") end
 
             cmd, state = iterate(command, state)
         end
@@ -78,7 +78,7 @@ function analyseLog(log::String)
 
 end
 
-function queryLog(dbpath::String, num::Int)
+function queryLog(dbpath::String, logidx::Int)
 
     if !isfile(dbpath)
         error("Database not found!")
@@ -90,10 +90,10 @@ function queryLog(dbpath::String, num::Int)
         DBInterface.execute(db, "SELECT id, content FROM records LIMIT 100;")
     )
 
-    println(table.id[num])
+    println(table.id[logidx])
 
     # decompress tenhou log contents and return processed table
-    return transcode(LZ4FrameDecompressor, table.content[num])
+    return String(transcode(LZ4FrameDecompressor, table.content[logidx]))
 end
 
-analyseLog(String(queryLog("scraw2009s4p.db", 43)))
+analyseLog(queryLog("scraw2009s4p.db", 43))
